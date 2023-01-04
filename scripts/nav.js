ad057eddc908d635f9bdcc928cd40d8dc9fb7f21
a-box31 <thomaa2031@gmail.com>
2023-01-04 17:12:59 -0500

    Setting up tab styling
    Added a side tab that appears when screen size is smaller than 800px. Must make this functional with the hamburger button.

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,12 +4,18 @@
 
 window.onload = function() {
      
-    window.addEventListener('scroll', function(e) {
-        if (window.pageYOffset > 500) {
-            document.querySelector(".main-header").classList.add('is-scrolling');
-        } else {
-            document.querySelector(".main-header").classList.remove('is-scrolling');
-        }
-    }); 
+    // window.addEventListener('scroll', function(e) {
+    //     if (window.pageYOffset > 500) {
+    //         document.querySelector(".main-header").classList.add('is-scrolling');
+    //     } else {
+    //         document.querySelector(".main-header").classList.remove('is-scrolling');
+    //     }
+    // }); 
 
+    const menu_btn = document.querySelector(".hamburger");
+    menu_btn.addEventListener('click', function() {
+        menu_btn.classList.toggle('is-active')
+    });
+
+    
 }
